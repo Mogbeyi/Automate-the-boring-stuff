4800b088f02ff7504baf7ebccd3392d9752b1050
Mogbeyi <mogbeyiwenyinmi@gmail.com>
2020-11-16 13:59:04 +0100

    Add project

---
+++ regex/regex_practice.py
@@ -0,0 +1,20 @@
+import re
+
+text = 'Robocop eats baby food. BABY FOOD.'
+vowels = 'aeiouAEIOU'
+
+def vowel_regex_finder():
+    vowelRegex = re.compile(r'[aeiouAEIOU]')
+    return vowelRegex.findall(text)
+
+def consonant_regex_finder():
+    consonant_regex = re.compile(r'[^aeiouAEIOU]')
+    return consonant_regex.findall(text)
+
+def vowel_normal_finder():
+    return [char for char in text if char in vowels]
+
+print(vowel_regex_finder())
+print(vowel_normal_finder())
+print(consonant_regex_finder())
+
