fea9d61f4d22a3bf293689123742018ad102f519
Mogbeyi <mogbeyiwenyinmi@gmail.com>
2020-11-18 00:41:27 +0100

    Refactor with modules

---
+++ regex/phoneAndEmail/email.py
@@ -0,0 +1,17 @@
+import re 
+
+# Create email regex. 
+email_regex = re.compile(r'''(
+[a-zA-Z0-9._%+-]+           # username
+@                           # symbol
+[a-zA-Z0-9.-]+              # domain name
+(\.[a-zA-Z]{2,4})           # dot-something
+)''', re.VERBOSE)
+
+def get_email_adresses(text):
+    email_matches = []
+
+    for groups in email_regex.findall(text):
+        email_matches.append(groups[0])
+
+    return email_matches
---
+++ regex/phoneAndEmail/phoneAndEmail.py
@@ -0,0 +1,21 @@
+from phone import *
+from email import *
+import pyperclip
+
+def main():
+    text = str(pyperclip.paste()) 
+    phone_numbers = get_phone_numbers(text)
+    emails = get_email_adresses(text)
+    numbers_and_emails = phone_numbers + emails
+
+    if len(numbers_and_emails) > 0:
+        finalMatch = '\n'.join(numbers_and_emails)
+        pyperclip.copy(finalMatch)
+        print('Copied to clipboard:')
+        print(finalMatch)
+    else:
+        print("No phone numbers or email adresses found.")
+
+if __name__ == '__main__':
+    main()
+
